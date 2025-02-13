30c4c6e8251e3f2a785bc38ec03d95440394571f
harrisyoung27 <138505736+harrisyoung27@users.noreply.github.com>
2025-02-14 01:27:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function first() {
+    let audio = new Audio("./audio/masarapdawmanulak.mp3");
+    audio.play();
+}
+function second() {
+    let audio = new Audio("./audio/emeemelangyon.mp3");
+    audio.play();
+}
+function third() {
+    let audio = new Audio("./audio/bastadinyaalam.mp3");
+    audio.play();
+}
+function fourth() {
+    let audio = new Audio("./audio/sigekwentomoyan.mp3");
+    audio.play();
+}
+function fifth() {
+    let audio = new Audio("./audio/hatdognanakatayo.mp3");
+    audio.play();
+}
+function sixth() {
+    let audio = new Audio("./audio/eueuauauae.mp3");
+    audio.play();
+}
+function seventh() {
+    let audio = new Audio("./audio/manonooddnghentai.mp3");
+    audio.play();
+}
+function eighth() {
+    let audio = new Audio("./audio/masmadalimagparol.mp3");
+    audio.play();
+}
